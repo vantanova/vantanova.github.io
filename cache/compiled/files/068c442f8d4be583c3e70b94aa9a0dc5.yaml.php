c5062144192ec4522ea6a472bc0e92ff24915a61
Antonio <anguilar@aol.com>
2021-04-11 20:46:53 -0700

    added initial files

--- cache/compiled/files/068c442f8d4be583c3e70b94aa9a0dc5.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/build/grav/user/config/security.yaml',
-    'modified' => 1616189108,
-    'data' => [
-        'salt' => '9AqlVuwmD86P5Y'
-    ]
-];
